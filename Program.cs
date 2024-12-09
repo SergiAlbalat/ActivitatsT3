6193e58b2298fddbc53a664a7045ab953816ce7a
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2024-12-09 11:53:15 +0100

    Exercici 1 fet Closes #1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+namespace ActivitatsT3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Player jugador1 = new Player("Messi", 30);
+            Team equip1 = new Team("Barça");
+            Console.WriteLine("{0}, {1}, {2}", jugador1.Name, jugador1.Age, equip1.Name);
+        }
+    }
+}
