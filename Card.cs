76b65a7be278ff9afed52e78da6d1cdcd6710712
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2024-12-10 13:50:46 +0100

    Exercici 6 Completat Closes #10

---
+++ Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ActivitatsT3
+{
+    public class Card
+    {
+        public string Type { get; set; }
+
+        public int Value { get; set; }
+        
+        public Card(string type, int value)
+        {
+            Type = type;
+            Value = value;
+        }
+        public Card(string type) : this(type, 0) { }
+        public Card(int value) : this("Unspecified", value) { }
+        public Card() : this("Unspecified", 0) { }
+    }
+}
---
+++ Flight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ActivitatsT3
+{
+    public class Flight
+    {
+        public int Duration {  get; set; }
+
+        public string Origin { get; set; }
+
+        public string Destination { get; set; }
+
+        public string Company { get; set; }
+
+        public Flight(int duration, string origin, string destination, string company)
+        {
+            Duration = duration;
+            Origin = origin;
+            Destination = destination;
+            Company = company;
+        }
+    }
+}
