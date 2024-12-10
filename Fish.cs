a9e29e7abc6975a8ad6ae994a677659a96a6eaf5
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2024-12-10 10:42:14 +0100

    Exercici 4 acabat Closes #4

---
+++ Fish.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ActivitatsT3
+{
+    public class Fish : Animal
+    {
+        public bool Sea {  get; set; }
+        public Fish(bool sea) : base("Fish", 0)
+        {
+            Sea = sea;
+        }
+    }
+}
