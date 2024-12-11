659b0e26778239af2d0d44adccd1ea0691e96433
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2024-12-11 10:26:37 +0100

    Perdon Closes #13

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ActivitatsT3
         {
             Cat gatete = new Cat("Morgana", 1, "negro", "atun");
             Console.WriteLine("{0}, {1}, {2}, {3}", gatete.Name, gatete.Age, gatete.Breed, gatete.FavouriteFood);
-            gatete.Eat();
+            gatete.Eat(); 
         }
     }
 }
