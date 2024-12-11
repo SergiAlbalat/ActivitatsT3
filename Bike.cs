353b8cec40c87e165fb7f1b856a3b813d98a5ddf
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2024-12-11 10:06:56 +0100

    Exercici 7 Completat Closes #12

---
+++ Bike.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ActivitatsT3
+{
+    public class Bike
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int WheelSize { get; set; }
+        public int GearNumber { get; set; }
+        public Bike(string name, string color, int wheelSize, int gearNumber)
+        {
+            Name = name;
+            Color = color;
+            WheelSize = wheelSize;
+            GearNumber = gearNumber;
+        }
+    }
+}
