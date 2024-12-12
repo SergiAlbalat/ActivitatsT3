9d5884904b14252d269d0f0f03f05cc828c1e17c
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2024-12-12 13:48:34 +0100

    Exercici 14 fet Closes #27

---
+++ FurnitureEx14.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ActivitatsT3
+{
+    public class FurnitureEx14
+    {
+        public float Weight {  get; set; }
+        public double Price {  get; set; }
+        public bool Set {  get; set; }
+        public string Name { get; set; }
+        public string Material { get; set; }
+        public string Colour {  get; set; }
+        public FurnitureEx14(float weight, double price, bool set, string name, string material, string colour)
+        {
+            Weight = weight;
+            Price = price;
+            Set = set;
+            Name = name;
+            Material = material;
+            Colour = colour;
+        }
+        public FurnitureEx14() : this(0.0f, 0, true, "Furniture", "Wood", "Brown") { }
+    }
+
+}
