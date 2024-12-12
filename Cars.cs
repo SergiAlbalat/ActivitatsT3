8886e0f110681dd8b987dd979d9dcb88c9597791
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2024-12-12 12:15:28 +0100

    Exercici 10 Completat Closes #19

---
+++ Cars.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ActivitatsT3
+{
+    public class Cars : WheeledVehicle
+    {
+        public string FuelType {  get; set; }
+        Cars(string name, string color, int wheelSize, string fuelType) : base(name, color, wheelSize)
+        {
+            FuelType = fuelType;
+        }
+    }
+}
---
+++ WheeledVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ActivitatsT3
+{
+    public class WheeledVehicle : Vehicles
+    {
+        public int WheelSize { get; set; }
+        public WheeledVehicle(string name, string color, int wheelSize) : base(name, color)
+        {
+            WheelSize = wheelSize;
+        }
+        public WheeledVehicle(string name, int wheelSize) : base(name)
+        {
+            WheelSize = wheelSize;
+        }
+    }
+}
