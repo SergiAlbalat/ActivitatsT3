b9e31b213094f66533086bdfdafc8f7c0c3ae2b2
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-01-16 13:45:09 +0100

    Exercici21 fet Closes #41

---
+++ IVehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ActivitatsT3
+{
+    public interface IVehicle
+    {
+        void Drive();
+        bool Refuel(int amount);
+    }
+}
