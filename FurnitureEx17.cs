1f5bd17ae71b0a644c16b77642db22d67b46411b
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-01-16 12:41:44 +0100

    Exercici 18 Completat Closes #32

--- FurnitureEx17.cs
+++ FurnitureEx17.cs
@@ -1,27 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace ActivitatsT3
 {
-    public class FurnitureEx17
+    public class FurnitureEx17 : HouseParts
     {
         public float Weight { get; set; }
-        public double Price { get; set; }
         public bool Set {  get; set; }
-        public string? Name { get; set; }
         public string? Material { get; set; }
         public string? Colour { get; set; }
-        public FurnitureEx17(float weight, double price, bool set,  string? name, string? material, string? colour)
+        public FurnitureEx17(float weight, double price, bool set,  string? name, string? material, string? colour) : base(name,  price)
         {
             Weight = weight;
-            Price = price;
             Set = set;
-            Name = name;
             Material = material;
             Colour = colour;
         }
         public FurnitureEx17() : this(0.00f, 0.00, false, "furniture", null, null) { }
+        public override string GetInfo() => $"Name: {Name}, Price: {Price}, Weight: {Weight}, Set: {Set}, Material: {Material}, Colour: {Colour}";
     }
 }
