1c86eb9343d6405fc69687117b574bec940dd788
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-01-16 13:14:07 +0100

    Exercici 20 Completat Closes #38

---
+++ PaymentCard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ActivitatsT3
+{
+    public class PaymentCard
+    {
+        public double Balance { get; set; }
+        public PaymentCard(double startingBalance)
+        {
+            Balance = startingBalance;
+        }
+        public override string ToString() => $"The card has a balance of {Balance} euros";
+    }
+}
